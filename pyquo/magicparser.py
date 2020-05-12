8406a2a47c2c1a0f6c6f89950143b542e3b572b6
QuoLab Technologies <fabien.dombard@quolab.com>
2020-05-12 10:15:56 +0200

    Renamed magictool to magicparser to align with QuoLab naming

--- pyquo/magicparser.py
+++ pyquo/magicparser.py
@@ -1,16 +1,16 @@
 from .base import Model
 from .helper import TypeFactory
 
-MAGIC_TOOL_URL = "/v1/scan/text"
+MAGIC_PARSER_URL = "/v1/scan/text"
 
 
-class MagicTool(object):
+class MagicParser(object):
     @classmethod
     def parse(cls, text, session=None):
         session = session or Model._session
 
         payload = {'text': text}
-        res = session.http_post(MAGIC_TOOL_URL, json=payload).json()
+        res = session.http_post(MAGIC_PARSER_URL, json=payload).json()
 
         for i in res['records']:
             yield TypeFactory.deserialize(i)
